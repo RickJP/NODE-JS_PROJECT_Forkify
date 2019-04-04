ba3c955c670fc4d8a16e77987766d7634c308c2f
Rick <3741326+RickJP@users.noreply.github.com>
2019-04-04 12:12:20 +0900

    Recipe controller is functional in testing, but doesn't display on UI yet.

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const key2 = '3d02782f74a7fbc128e61977c8614a41';
+export const key = '64917b47003cd0a2a6db141b248e3ca4';
+
+export const proxy = 'http://cors-anywhere.herokuapp.com/';
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import { elements, renderLoader, clearLoader } from './views/base';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 
 /** Global state of app */
@@ -11,6 +12,11 @@ import * as searchView from './views/searchView';
  */
 
 const state = {};
+
+/**
+ *  SEARCH CONTROLLER
+ * 
+ */
 const controlSearch = async () => {
 	// Get query from view
 	const query = searchView.getInput(); //TODO
@@ -19,18 +25,23 @@ const controlSearch = async () => {
 		// New search object; Add to state
 		state.search = new Search(query);
 
-		// Prepare UI for results
-		searchView.clearInput();
-		searchView.clearResults();
+		try {
+			// Prepare UI for results
+			searchView.clearInput();
+			searchView.clearResults();
 
-		renderLoader(elements.searchRes);
+			renderLoader(elements.searchRes);
 
-		// Search for recipes
-		await state.search.getResults();
+			// Search for recipes
+			await state.search.getResults();
 
-		// Render results on UI
-		clearLoader();
-		searchView.renderResults(state.search.result);
+			// Render results on UI
+			clearLoader();
+			searchView.renderResults(state.search.result);
+		} catch (err) {
+			alert('Something is up with search.');
+			clearLoader();
+		}
 	}
 };
 
@@ -47,3 +58,36 @@ elements.searchResPages.addEventListener('click', (e) => {
 		searchView.renderResults(state.search.result, goToPage);
 	}
 });
+
+/**
+ *  RECIPE CONTROLLER
+ * 
+ */
+const controlRecipe = async () => {
+	// Get ID from url
+	const id = window.location.hash.replace('#', '');
+
+	// Prepare the UI for changes
+
+	//	Create a new recipe object
+	state.recipe = new Recipe(id);
+
+	try {
+		// Get recipe data
+		await state.recipe.getRecipe();
+
+		// Calculate servings and time
+		state.recipe.calcTime();
+		state.recipe.calcServings();
+
+		// Render recipe
+		console.log(state.recipe);
+	} catch (err) {
+		alert('Problem getting recipe');
+	}
+};
+
+// window.addEventListener('hashchange', controlRecipe);
+// window.addEventListener('load', controlRecipe);
+
+[ 'hasChange', 'load' ].forEach((event) => window.addEventListener(event, controlRecipe));
